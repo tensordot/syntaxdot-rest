077372606d2a4d0f0e8fa11a49d6c1f1ea47e76a
Daniël de Kok <me@danieldk.eu>
2020-09-22 13:08:31 +0200

    Add /tokens endpoint for tokenization
    Fixes #2

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,15 @@ async fn handle_annotations(mut request: Request<State>) -> tide::Result {
         .build())
 }
 
+async fn handle_tokens(mut request: Request<State>) -> tide::Result {
+    let tokens_reader =
+        SentenceStreamReader::new(request.take_body().into_reader().lines().sentences());
+
+    Ok(Response::builder(StatusCode::Ok)
+        .body(Body::from_reader(AsyncBufReader::new(tokens_reader), None))
+        .build())
+}
+
 #[derive(Clone)]
 struct State {
     annotator: Arc<Annotator>,
@@ -70,6 +79,7 @@ async fn main() -> anyhow::Result<()> {
     });
     app.at("/").get(|_| async { Ok("Hello, world!") });
     app.at("/annotations").post(handle_annotations);
+    app.at("/tokens").post(handle_tokens);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
