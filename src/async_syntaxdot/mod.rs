e47a897f14069fcba03cf55d3a44af8f6513dfc5
Daniël de Kok <me@danieldk.eu>
2021-06-11 10:58:33 +0200

    Add pipeline name to the sentence metadata

--- src/async_syntaxdot/mod.rs
+++ src/async_syntaxdot/mod.rs
@@ -1,6 +1,9 @@
 mod annotations;
 pub use annotations::{Annotations, ToAnnotations};
 
+mod metadata;
+pub use metadata::ToMetadata;
+
 mod unicode_cleanup;
 pub use unicode_cleanup::{ToUnicodeCleanup, UnicodeCleanup};
 
